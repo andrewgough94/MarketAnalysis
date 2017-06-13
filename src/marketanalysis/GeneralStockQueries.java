d6a1e55f5fa35cc061047c092c72c1f916e5a64a
andrewgough94 <andrewgough94@gmail.com>
2017-06-12 17:30:11 -0700

    Adding queries

--- src/marketanalysis/GeneralStockQueries.java
+++ src/marketanalysis/GeneralStockQueries.java
@@ -4,5 +4,8 @@ package marketanalysis;
  * Created by andrewgough94 on 6/12/2017.
  */
 public class GeneralStockQueries {
-    String numSecsStart2016 =
+    String numSecsStart2016 = "select count(distinct ticker) from Prices where day = (select max(distinct day) from Prices where Month(day) = 1 and Year(day) = 2016)";
+
+    String numSecsEnd2016 = "select count(distinct ticker) from Prices where day = (select max(distinct day) from Prices where Month(day) = 12 and Year(day) = 2016)";
+
 }
